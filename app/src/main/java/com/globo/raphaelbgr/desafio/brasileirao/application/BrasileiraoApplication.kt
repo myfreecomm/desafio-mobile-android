3f1146e757b0a95da1900340a5bd697669011815
Raphael Bernardo <raphaelbgr@gmail.com>
2020-04-06 16:37:42 -0300

    init timber log

--- app/src/main/java/com/globo/raphaelbgr/desafio/brasileirao/application/BrasileiraoApplication.kt
+++ app/src/main/java/com/globo/raphaelbgr/desafio/brasileirao/application/BrasileiraoApplication.kt
@@ -1,14 +1,24 @@
 package com.globo.raphaelbgr.desafio.brasileirao.application
 
 import android.app.Application
+import com.globo.raphaelbgr.desafio.brasileirao.BuildConfig
 import com.globo.raphaelbgr.desafio.brasileirao.application.di.ApplicationModule
 import com.globo.raphaelbgr.desafio.brasileirao.application.di.DaggerApplicationComponent
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 class BrasileiraoApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         injectDependencies()
+        initTimber()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
     }
 
     private fun injectDependencies() {
