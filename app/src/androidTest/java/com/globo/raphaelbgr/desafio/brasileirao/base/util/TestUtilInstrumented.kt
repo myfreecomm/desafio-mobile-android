99832b258b2c4fb43e86b0fc14bed2b54b68f531
Raphael Bernardo <raphaelbgr@gmail.com>
2020-04-11 17:59:31 -0300

    Instrumented test for MatchDetailsActivity

--- app/src/androidTest/java/com/globo/raphaelbgr/desafio/brasileirao/base/util/TestUtilInstrumented.kt
+++ app/src/androidTest/java/com/globo/raphaelbgr/desafio/brasileirao/base/util/TestUtilInstrumented.kt
@@ -1,5 +1,8 @@
 package com.globo.raphaelbgr.desafio.brasileirao.base.util
 
+import com.globo.raphaelbgr.desafio.data.network.response.MatchList
+import com.globo.raphaelbgr.desafio.data.network.response.matchlist.Match
+import com.google.gson.Gson
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
@@ -34,6 +37,20 @@ class TestUtilInstrumented {
             }
             inputStream?.close()
             return json.toString()
+        }
+
+        @Throws(IOException::class)
+        fun loadBrasileiraoMockSingleMatch(): Match {
+            val inputStream =
+                this.javaClass.classLoader!!.getResourceAsStream("brasileirao_mock_response.json")
+            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+            val json = StringBuilder()
+            var line: String?
+            while (bufferedReader.readLine().also { line = it } != null) {
+                json.append(line)
+            }
+            inputStream?.close()
+            return Gson().fromJson(json.toString(), MatchList::class.java).matchList?.get(0)!!
         }
     }
 }
