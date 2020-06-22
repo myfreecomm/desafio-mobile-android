cd0b0f83f4ef7dcb9bc6242280f2329f41ce76eb
raul.rae <raul.rae@policiacientifica.sp.gov.br>
2020-06-22 20:58:56 -0300

    offline cache implemented with simple validations

--- app/src/main/java/com/araujoraul/mvvmapp/ui/cart/CartFragment.kt
+++ app/src/main/java/com/araujoraul/mvvmapp/ui/cart/CartFragment.kt
@@ -24,16 +24,12 @@ class CartFragment : Fragment(R.layout.fragment_cart), ItemCartClickListener {
     private val txtTax by lazy { view?.findViewById<TextView>(R.id.taxValue) }
     private val txtSubtotal by lazy { view?.findViewById<TextView>(R.id.subtotalValue) }
     private val txtTotal by lazy { view?.findViewById<TextView>(R.id.totalValue) }
-    private lateinit var adapter: CartAdapter
+    private val adapter = CartAdapter(emptyList(), this)
     private val recyclerView by lazy { view?.findViewById<RecyclerView>(R.id.recyclerView_cart) }
     private val progressBar by lazy { view?.findViewById<ProgressBar>(R.id.progressBar_cart) }
     private val txtNoInternet by lazy { view?.findViewById<TextView>(R.id.noInternet) }
     private val fragManager by lazy { activity?.supportFragmentManager }
     private val fullDialog = DetailsDialogFragment()
-
-    init {
-        adapter = CartAdapter(emptyList(), this)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
