2b45be8b4395d617234d22474edb842a6db74657
derlandyb <d.belchior82@gmail.com>
2020-06-26 02:07:41 -0300

    Initial configuration and list screen layout

---
+++ app/src/main/java/br/com/derlandybelchior/nexaaschallenge/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.derlandybelchior.nexaaschallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
