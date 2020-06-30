0fe95b591b273bbb48282b668917dde2eadc9c3c
renanparis <paris.oliveira@gmail.com>
2020-06-29 22:22:33 -0300

    Add items layout list product

--- desafioandroid/src/main/java/com/renanparis/desafioandroid/ui/adapter/ProductsAdapter.kt
+++ desafioandroid/src/main/java/com/renanparis/desafioandroid/ui/adapter/ProductsAdapter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.renanparis.desafioandroid.R
 import com.renanparis.desafioandroid.data.model.Product
@@ -38,10 +37,11 @@ class ProductsAdapter(
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+
         private lateinit var product: Product
         private val nameField by lazy { itemView.name_product }
         private val priceField by lazy { itemView.price_product }
-        private val stockField by lazy { itemView.stock_product}
+        private val stockField by lazy { itemView.stock_product }
         private val imageField by lazy { itemView.image_product }
 
         init {
@@ -55,13 +55,37 @@ class ProductsAdapter(
         fun bind(product: Product) {
             this.product = product
             nameField.text = product.name
-            setImage(imageField, product.image_url)
-            stockField.text = product.stock.toString()
+            setImage(product.image_url)
+            setTextStock(product.stock)
             priceField.text = product.price.toString()
         }
 
-        private fun setImage(imageField: ImageView?, imageUrl: String) {
+        private fun setTextStock(stock: Int) {
+            when {
+                stock == 1 -> {
+                    stockField.text = ONE_LEFT
+                }
+                stock < 1 -> {
+                    stockField.text = MISSING_PRODUCT
+                }
+                else -> {
+                    stockField.text = IN_STOCK
+                }
+            }
+        }
+
+        private fun setImage(imageUrl: String) {
             Picasso.get().load(imageUrl).into(imageField)
         }
+
+
     }
+
+    companion object {
+        private const val ONE_LEFT = "only 1 left in stock"
+        private const val MISSING_PRODUCT = "missing product"
+        private const val IN_STOCK = "in stock"
+
+    }
+
 }
--- desafioandroid/src/main/java/com/renanparis/desafioandroid/ui/fragment/ProductsListFragment.kt
+++ desafioandroid/src/main/java/com/renanparis/desafioandroid/ui/fragment/ProductsListFragment.kt
@@ -5,10 +5,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.renanparis.desafioandroid.R
+import com.renanparis.desafioandroid.data.model.Product
 import com.renanparis.desafioandroid.ui.ProductsViewModel
 import com.renanparis.desafioandroid.ui.adapter.ProductsAdapter
 import com.renanparis.desafioandroid.utils.Status
@@ -20,6 +23,12 @@ class ProductsListFragment : Fragment() {
 
     private val viewModel: ProductsViewModel by viewModel()
     private val adapter: ProductsAdapter by inject()
+    lateinit var totalField: TextView
+    lateinit var subtotalField: TextView
+    lateinit var shippingField: TextView
+    lateinit var taxField: TextView
+    lateinit var button: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +43,11 @@ class ProductsListFragment : Fragment() {
                     Status.SUCCESS -> {
                         products_list_progress.visibility = View.GONE
                         products_list_rv.visibility = View.VISIBLE
-                        resource.data?.let { products-> adapter.update(products) }
+                        resource.data?.let { products->
+                            adapter.update(products)
+                            bindViews(products)
+                        }
+
                     }
                     Status.ERROR -> {
                         products_list_progress.visibility = View.GONE
@@ -61,16 +74,47 @@ class ProductsListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         configRecyclerView()
+        initViews()
+    }
+
+    private fun initViews() {
+        totalField = products_list_total_value
+        subtotalField = products_list_subtotal_value
+        shippingField = products_list_shipping_value
+        taxField = products_list_tax_value
+        button = products_list_button
+        initListener()
+    }
+
+    private fun initListener() {
+        button.setOnClickListener {
+            activity?.supportFragmentManager?.popBackStack()
+        }
+
     }
 
     private fun configRecyclerView() {
-
-//        val divisor = DividerItemDecoration(context, LinearLayout.HORIZONTAL)
-//        products_list_rv.addItemDecoration(divisor)
         adapter.onItemClickListener = {product ->
             Log.i("Nome", product.name)
         }
         products_list_rv.adapter = adapter
     }
+
+    private fun bindViews(products: List<Product>) {
+        var subtotal = 0
+        var shipping = 0
+        var tax = 0
+
+        for (product in products) {
+            subtotal += product.price
+            shipping += product.shipping
+            tax += product.tax
+        }
+        subtotalField.text = subtotal.toString()
+        shippingField.text = shipping.toString()
+        taxField.text = tax.toString()
+        totalField.text = (subtotal + shipping + tax).toString()
+    }
+
 
 }
