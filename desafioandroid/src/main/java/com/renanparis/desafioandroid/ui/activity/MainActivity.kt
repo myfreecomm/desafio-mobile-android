888c54fe592eac96f0e996b385918e50247b6ee3
renanparis <paris.oliveira@gmail.com>
2020-07-02 14:42:00 -0300

    Refactor

--- desafioandroid/src/main/java/com/renanparis/desafioandroid/ui/activity/MainActivity.kt
+++ desafioandroid/src/main/java/com/renanparis/desafioandroid/ui/activity/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
 import com.renanparis.desafioandroid.R
+import com.renanparis.desafioandroid.R.drawable.ic_close_black_24dp
+import com.renanparis.desafioandroid.R.drawable.ic_reorder_white_24dp
 
 class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
 
@@ -27,26 +31,34 @@ class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedList
         navController.addOnDestinationChangedListener(this)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val appBarConfig = AppBarConfiguration(navController.graph)
+        return navController.navigateUp(appBarConfig)  || super.onSupportNavigateUp()
+    }
+
     private fun setupActionBar() {
         val appBarConfig = AppBarConfiguration(setOf(R.id.productsListFragment, R.id.productDetailsFragment))
 
         if (listToolbar.visibility == View.VISIBLE) {
             setSupportActionBar(listToolbar)
             listToolbar.setupWithNavController(navController, appBarConfig)
+            listToolbar.navigationIcon = getDrawable(ic_reorder_white_24dp)
         } else if (detailToolbar.visibility == View.VISIBLE) {
             setSupportActionBar(detailToolbar)
             detailToolbar.setupWithNavController(navController, appBarConfig)
+            detailToolbar.navigationIcon = getDrawable(ic_close_black_24dp)
         }
     }
     override fun onDestinationChanged(controller: NavController, destination: NavDestination, arguments: Bundle?) {
         if (destination.id == R.id.productDetailsFragment) {
             listToolbar.visibility = View.GONE
             detailToolbar.visibility = View.VISIBLE
-            window.statusBarColor = resources.getColor(R.color.lightGray)
+            window.statusBarColor = ContextCompat.getColor(this,R.color.lightGray)
+
         } else {
             listToolbar.visibility = View.VISIBLE
             detailToolbar.visibility = View.GONE
-            window.statusBarColor = resources.getColor(R.color.colorPrimaryDark)
+            window.statusBarColor = ContextCompat.getColor(this,R.color.colorPrimaryDark)
         }
         setupActionBar()
     }
