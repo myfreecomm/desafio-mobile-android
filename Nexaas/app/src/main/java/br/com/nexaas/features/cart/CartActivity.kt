5df3e8f0cf8cf7399519e1aea49124c721ffa220
Ricarlo Silva <>
2020-07-08 02:58:11 -0300

    Add initial product details feature

--- Nexaas/app/src/main/java/br/com/nexaas/features/cart/CartActivity.kt
+++ Nexaas/app/src/main/java/br/com/nexaas/features/cart/CartActivity.kt
@@ -1,12 +1,15 @@
 package br.com.nexaas.features.cart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.nexaas.R
+import br.com.nexaas.features.product.ProductDetailsActivity
 
 class CartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
+        startActivity(Intent(this, ProductDetailsActivity::class.java))
     }
 }
---
+++ Nexaas/app/src/main/java/br/com/nexaas/features/product/ProductDetailsActivity.kt
@@ -0,0 +1,12 @@
+package br.com.nexaas.features.product
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.nexaas.R
+
+class ProductDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_details)
+    }
+}
