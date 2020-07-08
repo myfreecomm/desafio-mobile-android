12c61d7173dd29d7465a35a0e6ba68b26f0c6e00
Ricarlo Silva <>
2020-07-08 02:52:23 -0300

    Add initial cart feature

---
+++ Nexaas/app/src/main/java/br/com/nexaas/features/cart/CartActivity.kt
@@ -0,0 +1,12 @@
+package br.com.nexaas.features.cart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.nexaas.R
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
--- Nexaas/app/src/main/java/br/com/nexaas/features/main/MainActivity.kt
+++ Nexaas/app/src/main/java/br/com/nexaas/features/main/MainActivity.kt
@@ -1,11 +1,15 @@
-package br.com.nexaas
+package br.com.nexaas.features.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.nexaas.R
+import br.com.nexaas.features.cart.CartActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this, CartActivity::class.java))
     }
 }
